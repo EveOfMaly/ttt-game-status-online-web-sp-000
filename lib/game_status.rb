b5401a8a4be57276fa587b1c5196c50f956fdc8f
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-04-17 18:23:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,7 +33,7 @@ def won?(board)
      return win_combination
    elsif position_1 == " " && position_2 == " " && position_3 == " "
       return false
-  elsif draw?(board) 
+  else  draw?(board) 
       return false
    end
  end
